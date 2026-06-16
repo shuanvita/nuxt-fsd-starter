9069eaf3c2c26ea2f5f07d92260bde4f65d3061f
shuanvi7 <shuanvi7@gmail.com>
2026-06-16 14:45:12 +0300

    Установил модули @nuxt/image, @nuxt/scripts, @nuxt/eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -18,5 +17,5 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
 
-  modules: ['@nuxt/fonts'],
+  modules: ['@nuxt/fonts', '@nuxt/image', '@nuxt/scripts', '@nuxt/eslint'],
 });
