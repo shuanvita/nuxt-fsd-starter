591b7c18a93b23fd7c275bbbbeee50b1b39cc5b7
shuanvi7 <shuanvi7@gmail.com>
2026-06-16 14:08:35 +0300

    Установил и настроил Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -7,5 +9,9 @@ export default defineNuxtConfig({
     app: 'app/entrypoint',
     pages: 'app/routes',
     layouts: 'app/layouts',
+  },
+  css: ['./src/app/styles/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
